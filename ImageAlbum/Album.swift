814298ec92e7001fb7c7b7f8ef994f0b46267a04
div-antk <51283468+owaii@users.noreply.github.com>
2020-11-15 15:52:15 +0900

    アルバムビューの追加

--- ImageAlbum/Album.swift
+++ ImageAlbum/Album.swift
@@ -9,10 +9,15 @@ import SwiftUI
 
 struct Album: View {
   var body: some View {
+    
     NavigationView {
       VStack {
-        Text("すべての項目")
-        Text("お気に入り")
+        AlbumView(name: "すべての項目",
+                  path: "",
+                  isFavorite: false)
+        AlbumView(name: "お気に入り",
+                  path: "",
+                  isFavorite: true)
       }
       // VStack{}にnavigationBarTitle()を追加
       .navigationTitle(Text("アルバム"))
