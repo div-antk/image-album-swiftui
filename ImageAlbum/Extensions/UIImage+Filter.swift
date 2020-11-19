aebcaa2b6ea2f92f55b6676c18374f8dd7c7dde0
div-antk <51283468+owaii@users.noreply.github.com>
2020-11-20 08:18:14 +0900

    セピア処理の実装

--- ImageAlbum/Extensions/UIImage+Filter.swift
+++ ImageAlbum/Extensions/UIImage+Filter.swift
@@ -53,5 +53,52 @@ extension UIImage {
     return grayScaleImage
   }
   
-
+  // セピア（クラスメソッド）
+  class func sepia(path: String) -> UIImage {
+    
+    // セピアメソッドに渡されたURLからCIImageを作成
+    let ciImageWrap: CIImage? = CIImage(contentsOf: URL(fileURLWithPath: path))
+    
+    guard let ciImage = ciImageWrap else {
+      return UIImage.init()
+    }
+    
+    // CIFiterでセピア設定
+    let ciFilterWrap: CIFilter? = CIFilter(name: "CISepiaTone")
+    
+    guard let ciFilter = ciFilterWrap else {
+      return UIImage.init()
+      
+    }
+    
+    // kCIInputImageKeyでフィルターをかける処理をciImageに設定
+    ciFilter.setValue(ciImage, forKey: kCIInputImageKey)
+    // inputIntensityでセピア処理にかける度合いを1.0に設定
+    ciFilter.setValue(1.0, forKey: "inputIntensity")
+    
+    // CIImageでセピア画像作成
+    let sepiaCIImageWrap: CIImage? = ciFilter.outputImage
+    
+    guard let sepiaCIImage = sepiaCIImageWrap else {
+      return UIImage.init()
+    }
+    
+    // CIImage -> CGImage
+    let ciContext: CIContext = CIContext(options: nil)
+    let cgImageWrap: CGImage? = ciContext.createCGImage(sepiaCIImage,
+                                                        from: sepiaCIImage.extent)
+    
+    guard let cgImage = cgImageWrap else {
+      return UIImage.init()
+    }
+    
+    // CGImageをビューで表示できるUIImageに変換
+    let sepiaImageWrap: UIImage? = UIImage(cgImage: cgImage, scale: 0.0,
+                                           orientation: UIImage.Orientation.up)
+    
+    guard let sepiaImage = sepiaImageWrap else {
+      return UIImage.init()
+    }
+    return sepiaImage
+  }
 }
