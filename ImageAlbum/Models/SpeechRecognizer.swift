2bbb6ed294875c2fb78a37bb68d4e1150a474d05
div-antk <51283468+owaii@users.noreply.github.com>
2020-11-22 23:25:38 +0900

    音声認識のメソッドの概要

--- ImageAlbum/Models/SpeechRecognizer.swift
+++ ImageAlbum/Models/SpeechRecognizer.swift
@@ -11,16 +11,17 @@ import Speech // 音声認識フレームワーク
 //MARK:- Speech
 
 class SpeechRecognizer {
-  // 音を管理
+  // 音を管理（端末のマイクから音声データをバッファ［一時的なメモリ領域］に取得）
   private let audioEngine = AVAudioEngine()
   
   // マイクから取得した音声バッファを音声認識に渡す
+  // （上記で取得した音声バッファを SFSpeechRecognizer に渡す）
   private var speechRequest: SFSpeechAudioBufferRecognitionRequest?
   
-  // 音声認識を日本語に設定
+  // 音声認識を日本語に設定（渡された音声バッファを解析して文字列を生成）
   private var speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "ja-JP"))!
   
-  // 音声認識タスクを生成
+  // 音声認識タスクを生成（音声認識の進行状況を監視するために使用するタスク［スレッド］）
   private var speechTask: SFSpeechRecognitionTask?
   
   // ユーザに音声認識の許可を促す
@@ -28,7 +29,6 @@ class SpeechRecognizer {
     SFSpeechRecognizer.requestAuthorization { (authStatus) in
       
       switch authStatus {
-      
       case .authorized:
         // ユーザが許可
         print("許可")
@@ -52,9 +52,9 @@ class SpeechRecognizer {
   }
 
   // 音声認識を開始する
+  // 音声認識タスク（スレッド）を常に1つにするため、すでに開始している場合はキャンセルする
   func start(completion: @escaping (Bool) -> Void) throws {
     
-    // 音声認識タスクがすでに開始している場合キャンセルする
     if let speechTask = speechTask {
       speechTask.cancel()
       self.speechTask = nil
@@ -66,7 +66,9 @@ class SpeechRecognizer {
     self.speechRequest = SFSpeechAudioBufferRecognitionRequest()
     
     // 音声認識タスク
+    // 音声バッファが解析されると{}内の処理が実行される
     self.speechTask = self.speechRecognizer.recognitionTask(with: self.speechRequest!) {
+      // 弱参照（weak）selfの判定
       [weak self](result, error) in
       
       // エラー判定
@@ -86,7 +88,7 @@ class SpeechRecognizer {
         // デバッグログに音声認識結果を表示する
         print(seg!.substring)
         
-        // 回れと認識した場合
+        // 音声認識の最新結果が「回れ」と認識した場合
         if seg!.substring == "回れ" {
           // クロージャにして音声認識フラグをtrueにする
           completion(true)
@@ -109,7 +111,8 @@ class SpeechRecognizer {
     // 音の出力フォーマットを取得する
     let recordFormat = self.audioEngine.inputNode.outputFormat(forBus: 0)
     
-    // 前回実行時に監視していたバスを削除する
+    // 前回実行時に監視していたバス（音の出る場所）を削除している
+    // 2回目以降に実行した場合、同じバスを設定できないため
     self.audioEngine.inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordFormat) {
       [weak self](buffer: AVAudioPCMBuffer, when: AVAudioTime) in
       
@@ -120,7 +123,7 @@ class SpeechRecognizer {
       // 音声バッファを追加
       self.speechRequest?.append(buffer)
     }
-    // マイク音取得を開始
+    // マイクから音を取得
     self.audioEngine.prepare()
     try? self.audioEngine.start()
   }
